da263b983fdd0506340d325fde8d9fb60e048e86
witdory <yoonbal0406@naver.com>
2025-12-14 02:47:49 +0900

    fix: producer와 consumer간의 데이터 전달 형식 수정

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ app = EdgeApp("test-app")
 
 @app.producer(fps=10)
 def camera():
-    # 320x240 랜덤 노이즈 이미지 생성
     frame = np.random.randint(0, 255, (240, 320, 3), dtype=np.uint8)
-    return frame
+    
+
+    _, buf = cv2.imencode('.jpg', frame)
+    return buf.tobytes()
 
 @app.consumer(replicas=1)
-def ai(frame):
-    # 이미지에 "Processed" 글자 쓰기
+def ai(frame_bytes):
+    # [핵심 수정] 받은 데이터는 bytes이므로, 다시 이미지(Numpy)로 복원해야 함
+    nparr = np.frombuffer(frame_bytes, np.uint8)
+    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
     cv2.putText(frame, "Processed", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    
     _, buf = cv2.imencode('.jpg', frame)
     return buf.tobytes()
 
